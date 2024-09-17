0820b8b03c8feeebd3fd4678cc2f52051d2ffad5
M.can <caglayan.mertcan37@gmail.com>
2024-09-17 15:21:16 +0300

    add our-products-section

--- src/app/components/home/best-selling-section/best-selling-section.component.ts
+++ src/app/components/home/best-selling-section/best-selling-section.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { CardComponent } from "../../card/card.component";
+import { CardComponent } from '../../card/card.component';
 
 @Component({
   selector: 'app-best-selling-section',
   standalone: true,
   imports: [CardComponent],
   templateUrl: './best-selling-section.component.html',
-  styleUrl: './best-selling-section.component.scss'
+  styleUrl: './best-selling-section.component.scss',
 })
 export class BestSellingSectionComponent {
-
+  cardType: string = 'best-selling-section';
+  
 }
