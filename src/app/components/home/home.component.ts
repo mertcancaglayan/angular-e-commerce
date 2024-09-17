8f15e89a2004ac6eb12911d9c6665d4eaeffcf7e
M.can <caglayan.mertcan37@gmail.com>
2024-09-17 18:19:25 +0300

    add offer-section, count down

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../layouts/header/header.component";
 import { BannerComponent } from "./banner/banner.component";
 import { BestSellingSectionComponent } from "./best-selling-section/best-selling-section.component";
 import { OurProductsSectionComponent } from "./our-products-section/our-products-section.component";
+import { OfferSectionComponent } from "./offer-section/offer-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, BestSellingSectionComponent, OurProductsSectionComponent],
+  imports: [HeaderComponent, BannerComponent, BestSellingSectionComponent, OurProductsSectionComponent, OfferSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
