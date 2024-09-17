4722e2c756aacd3fac9840cc15f524dcbeb0c818
M.can <caglayan.mertcan37@gmail.com>
2024-09-17 18:45:49 +0300

    add recommendation section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { BannerComponent } from "./banner/banner.component";
 import { BestSellingSectionComponent } from "./best-selling-section/best-selling-section.component";
 import { OurProductsSectionComponent } from "./our-products-section/our-products-section.component";
 import { OfferSectionComponent } from "./offer-section/offer-section.component";
+import { RecommendationSectionComponent } from "./recommendation-section/recommendation-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, BestSellingSectionComponent, OurProductsSectionComponent, OfferSectionComponent],
+  imports: [HeaderComponent, BannerComponent, BestSellingSectionComponent, OurProductsSectionComponent, OfferSectionComponent, RecommendationSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/components/home/recommendation-section/recommendation-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './recommendation-section.component.html',
+  styleUrl: './recommendation-section.component.scss'
+})
+export class RecommendationSectionComponent {
+
+}
