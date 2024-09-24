16a11f59492191146d6b37bef72d3656a0df1922
M.can <caglayan.mertcan37@gmail.com>
2024-09-24 14:30:09 +0300

    update card style, categories style. Further Implement Api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../layouts/header/header.component";
-import { BannerComponent } from "./banner/banner.component";
-import { BestSellingSectionComponent } from "./best-selling-section/best-selling-section.component";
-import { OurProductsSectionComponent } from "./our-products-section/our-products-section.component";
-import { OfferSectionComponent } from "./offer-section/offer-section.component";
-import { RecommendationSectionComponent } from "./recommendation-section/recommendation-section.component";
-import { TestimonialSectionComponent } from "./testimonial-section/testimonial-section.component";
-import { FooterComponent } from "../layouts/footer/footer.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../layouts/header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { BestSellingSectionComponent } from './best-selling-section/best-selling-section.component';
+import { OurProductsSectionComponent } from './our-products-section/our-products-section.component';
+import { OfferSectionComponent } from './offer-section/offer-section.component';
+import { RecommendationSectionComponent } from './recommendation-section/recommendation-section.component';
+import { TestimonialSectionComponent } from './testimonial-section/testimonial-section.component';
+import { FooterComponent } from '../layouts/footer/footer.component';
+import { FakeStoreApiService } from '../../services/fake-store-api.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, BestSellingSectionComponent, OurProductsSectionComponent, OfferSectionComponent, RecommendationSectionComponent, TestimonialSectionComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    BannerComponent,
+    BestSellingSectionComponent,
+    OurProductsSectionComponent,
+    OfferSectionComponent,
+    RecommendationSectionComponent,
+    TestimonialSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private apiService: FakeStoreApiService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.apiService.getAllProducts().subscribe(
+      (data) => {
+        this.products = data;
+        console.log(this.products);
+      },
+      (error) => {
+        console.error('Error fetching products', error);
+      }
+    );
+  }
+}
