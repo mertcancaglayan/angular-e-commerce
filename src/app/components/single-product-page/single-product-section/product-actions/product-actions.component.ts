bb19c2293050b73c5865c0caf0da6d90049be966
M.can <caglayan.mertcan37@gmail.com>
2024-09-26 14:29:21 +0300

    add cart-service, cart model

--- src/app/components/single-product-page/single-product-section/product-actions/product-actions.component.ts
+++ src/app/components/single-product-page/single-product-section/product-actions/product-actions.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../../../services/cart.service';
+import { CartProduct } from '../../../../models/cart.model';
 
 @Component({
   selector: 'app-product-actions',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-actions.component.html',
-  styleUrl: './product-actions.component.scss',
+  styleUrls: ['./product-actions.component.scss'],
 })
 export class ProductActionsComponent {
+  @Input() productId!: number;
   quantity: number = 1;
+  cartId: number = 1;
+
+  constructor(private cartService: CartService) {}
 
   decreaseQuantity() {
     if (this.quantity > 1) {
@@ -21,7 +28,19 @@ export class ProductActionsComponent {
   }
 
   addToCart() {
-    console.log(`Added ${this.quantity} item(s) to cart.`);
+    const cartProduct: CartProduct = {
+      productId: this.productId,
+      quantity: this.quantity,
+    };
+
+    this.cartService.updateCart(this.cartId, [cartProduct]).subscribe({
+      next: (response) => {
+        response;
+      },
+      error: (error) => {
+        console.error('Error updating cart:', error);
+      },
+    });
   }
 
   buyNow() {
---
+++ src/app/models/cart.model.ts
@@ -0,0 +1,11 @@
+export interface CartProduct {
+  productId: number;
+  quantity: number;
+}
+
+export interface Cart {
+  id: number;
+  userId: number;
+  date: string;
+  products: CartProduct[];
+}
